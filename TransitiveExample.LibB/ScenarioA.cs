fd01c3486fe691d4a77566744597e5c190ae66e4
Chris Weldon <chris.weldon@wolterskluwer.com>
2021-09-08 21:38:25 -0500

    Creating LibB with ScenarioA.

---
+++ TransitiveExample.LibB/ScenarioA.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TransitiveExample.LibB
+{
+    public class ScenarioA
+    {
+        public void Execute()
+        {
+            Console.WriteLine("Executing TransitiveExample.LibB.ScenarioA.Execute()");
+        }
+    }
+}
